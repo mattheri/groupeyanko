5a0217d63c81d3b1dcc0358540f1b396c5d502e5
Mathieu Thériault <mathieu.theriault89@gmail.com>
2021-01-18 07:56:45 -0500

    layout changes

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from './footer.module.scss';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import cn from 'classnames';
 
 export function Footer() {
     return (
-        <Row as='footer' className={cn({
-            [styles.footer]: styles.footer,
-            ['px-0 mx-0']: true
-        })}>
+        <Container fluid className='px-0 mx-0' as='footer'>
+            <Row className={cn({
+                [styles.footer]: styles.footer,
+                ['px-0 mx-0']: true
+            })}>
 
-        </Row>
+            </Row>
+        </Container>
     );
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import { Squash as Hamburger, Squash } from 'hamburger-react';
 import cn from 'classnames';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 export function Navbar() {
     const [appState, setAppState]: AppContextTuple = React.useContext(AppContext);
@@ -34,39 +35,41 @@ export function Navbar() {
     const handleChangeLocale = () => setAppState(state => Object.assign({}, state, { locale: appState.locale === 'en' ? 'fr' : 'en' }));
 
     return (
-        <Row as='nav' className={styles.navbar}>
-            <Col xs={12} className='d-flex align-items-center '>
-                <a className={styles.brand} href='https://proaxion.ca/en/home/'>
-                    <Image
-                        src='/uploads/images/logo-PROAXION.png'
-                        width={789} height={170}
-                        alt='Proaxion Logo'
-                        layout='intrinsic' />
-                </a>
-                <div className={cn(styles.links, styles.desktop)}>
-                    <Button href="https://proaxion.ca/en/home/" tertiary text={text[appState.locale].home} />
-                    <Button href="/" tertiary text={text[appState.locale].catalog} />
-                    <Button href="/contact" tertiary text={text[appState.locale].contact} />
-                    <Cart />
-                    <Button href='/' text={text[appState.locale].login} />
-                </div>
-                <div className={cn(styles.cartMobile, styles.mobile)}>
-                    <Cart />
-                </div>
-                <div className={cn(styles.mobileButton, styles.mobile)}>
-                    <Squash toggled={show} toggle={setShow} color='#ffffff' />
-                </div>
-                <div className={cn({
-                    [styles.linksMobile]: true,
-                    [styles.mobile]: true,
-                    [styles.show]: show
-                })}>
-                    <a onClick={() => setShow(false)} href="https://proaxion.ca/en/home/">{text[appState.locale].home}</a>
-                    <Link href='/'><a onClick={() => setShow(false)}>{text[appState.locale].catalog}</a></Link>
-                    <Link href='/contact'><a onClick={() => setShow(false)}>{text[appState.locale].contact}</a></Link>
-                    <Button href='/' onClick={() => setShow(false)} text={text[appState.locale].login} />
-                </div>
-            </Col>
-        </Row>
+        <Container as='nav' fluid>
+            <Row className={styles.navbar}>
+                <Col xs={12} className='d-flex align-items-center '>
+                    <a className={styles.brand} href='https://proaxion.ca/en/home/'>
+                        <Image
+                            src='/uploads/images/logo-PROAXION.png'
+                            width={789} height={170}
+                            alt='Proaxion Logo'
+                            layout='intrinsic' />
+                    </a>
+                    <div className={cn(styles.links, styles.desktop)}>
+                        <Button href="https://proaxion.ca/en/home/" tertiary text={text[appState.locale].home} />
+                        <Button href="/" tertiary text={text[appState.locale].catalog} />
+                        <Button href="/contact" tertiary text={text[appState.locale].contact} />
+                        <Cart />
+                        <Button href='/' text={text[appState.locale].login} />
+                    </div>
+                    <div className={cn(styles.cartMobile, styles.mobile)}>
+                        <Cart />
+                    </div>
+                    <div className={cn(styles.mobileButton, styles.mobile)}>
+                        <Squash toggled={show} toggle={setShow} color='#ffffff' />
+                    </div>
+                    <div className={cn({
+                        [styles.linksMobile]: true,
+                        [styles.mobile]: true,
+                        [styles.show]: show
+                    })}>
+                        <a onClick={() => setShow(false)} href="https://proaxion.ca/en/home/">{text[appState.locale].home}</a>
+                        <Link href='/'><a onClick={() => setShow(false)}>{text[appState.locale].catalog}</a></Link>
+                        <Link href='/contact'><a onClick={() => setShow(false)}>{text[appState.locale].contact}</a></Link>
+                        <Button href='/' onClick={() => setShow(false)} text={text[appState.locale].login} />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
