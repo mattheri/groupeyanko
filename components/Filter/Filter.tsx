a2889c9b87645b1418ba37eab46f7afba1275a66
Mathieu Thériault <mathieu.theriault89@gmail.com>
2021-01-20 21:35:50 -0500

    remove rogue page

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Button } from '../Button/Button';
-import { AppContext, AppContextTuple } from '../Context/AppContext';
 import Link from 'next/link';
 import styles from './filter.module.scss';
 import Col from 'react-bootstrap/Col';
@@ -42,19 +41,24 @@ export function Filter() {
 
     React.useEffect(() => {
         (async () => {
-            const response = (await axios.get('/api/categories')).data;
-            setCategories(response);
-
-            console.log(categories);
+            if (!categories) {
+                const response = (await axios.get('/api/categories')).data;
+                setCategories(response);
+            }
         })();
-    }, [])
+    }, []);
 
     return (
         <>
             <Button className={styles.filterBtn} onClick={handleOpen}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                </svg>
+                {open ?
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg> :
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                }
             </Button>
             <AnimatePresence>
                 {open && categories.length > 0 &&
@@ -63,14 +67,15 @@ export function Filter() {
                         as={motion.aside}
                         layout
                         initial={{ x: -100, opacity: 0 }}
-                        animate={{ x: 0, opacity: 1 }}
+                        animate={{ x: 40, opacity: 1 }}
                     exit={{ x: -100, opacity: 0 }}
                     layoutId={''}
                         xs={10}
                         md={6}
                         lg={2}
-                        className={styles.filter}>
-                        <ul>
+                    className={styles.filter}>
+                    <ul>
+                        <li><input type="text" className={styles.search} /></li>
                             {handleSortCategories().map(parentCategory => {
                                 return (
                                     <li>
