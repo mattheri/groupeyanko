13c31e9f88fe681cbec04eec3f69dedb36c95e13
Mathieu Thériault <mathieu.theriault89@gmail.com>
2021-01-27 18:02:12 -0500

    fix previously introduced bug that would not load any pages other than those from categories, product or index

--- components/Breadcrumbs/Breadcrumbs.tsx
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -7,11 +7,40 @@ import { filter } from 'lodash';
 
 export function Breadcrumbs() {
     const router = useRouter();
-    const [navigationState, setNavigationState]: NavigationContextTuple = React.useContext(NavigationContext);
+    const [navigationState, setNavigationState] = React.useState({
+        [`Accueil`]: '/'
+    })
+
+    React.useEffect(() => {
+        console.log(router);
+        setNavigationState(state => {
+
+            if (router.asPath.includes("category")) {
+                return {
+                    [`Accueil`]: '/',
+                    [`Catégorie`]: router.asPath || '/'
+                }
+            }
+
+            if (router.asPath.includes("product")) {
+                return Object.assign(
+                    {},
+                    state,
+                    {
+                        ["Produit"]: router.asPath || '/'
+                    }
+                )
+            }
+
+            return {
+                [`Accueil`]: '/'
+            }
+        })
+    }, [router])
 
     return (
         <Breadcrumb className='mt-5'>
-            {Object.entries(navigationState).map(([key, value]) => <Breadcrumb.Item active={router.asPath === value}><Link href={value}>{key}</Link></Breadcrumb.Item>)}
+            {Object.entries(navigationState).map(([key, value]) => <Breadcrumb.Item key={key} active={router.asPath === value}><Link href={value}>{key}</Link></Breadcrumb.Item>)}
         </Breadcrumb>
     );
 }
