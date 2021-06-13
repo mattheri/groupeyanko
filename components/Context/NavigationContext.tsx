51f7f9693944042d3eacb963fbdd821f061a3471
Mathieu Thériault <mathieu.theriault89@gmail.com>
2021-06-13 19:25:31 -0400

    removed bad import

--- components/Context/NavigationContext.tsx
+++ components/Context/NavigationContext.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import React from "react";
-import { useWindow } from "../Hooks/useWindow";
 
 export const NavigationContext = React.createContext(null);
 
